05afd018da091f3e4123271e07743c16adaf43ab
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-02-20 02:24:13 +0100

    samples: add new sample: graph-test

---
+++ Samples/graph-test/graph-test/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using Gtk;
+
+public class MainWindow : Window
+{
+    static void Main(string[] args)
+    {
+        Application.Init ();
+
+        new MainWindow ();
+
+        Application.Run();
+    }
+
+    public MainWindow() : base("Plotter")
+    {
+        // Setup ui
+		Add (new Plotter (1.17, -5.2, 4.23));
+		ShowAll ();
+    }
+}
