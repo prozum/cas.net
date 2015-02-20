31fe9a2302509e033f8adfb97b9d03df37241ee3
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-02-20 11:42:48 +0100

    samples: restoring GraphviewTest

---
+++ Samples/GraphViewTest/GraphViewTest/GraphViewTest.cs
@@ -0,0 +1,20 @@
+﻿using Gtk;
+
+public class MainWindow : Window
+{
+    static void Main(string[] args)
+    {
+        Application.Init ();
+
+        new MainWindow ();
+
+        Application.Run();
+    }
+
+    public MainWindow() : base("GraphViewTest")
+    {
+        // Setup ui
+		Add (new GraphView (1.17, -5.2, 4.23));
+		ShowAll ();
+    }
+}
