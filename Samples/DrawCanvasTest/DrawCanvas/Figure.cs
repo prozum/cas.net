b763564a9ff93b677b6758bd9db7e2c695c1adb6
reindeernix <reindeernix@prozum.dk>
2015-02-25 23:34:56 +0100

    Added DrawCanvas test, it's like paint without colours and eraser

---
+++ Samples/DrawCanvasTest/DrawCanvas/Figure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Cairo;
+
+namespace Canvas
+{
+	public class Figure
+	{
+		private List<double> X;
+		private List<double> Y;
+
+		public Figure(double x, double y)
+		{
+			X = new List<double>();
+			Y = new List<double>();
+
+			X.Add(x);
+			Y.Add(y);
+		}
+
+		public void Add(double x, double y)
+		{
+			X.Add(x);
+			Y.Add(y);
+		}
+
+		public void DrawFigure(ref Context ctx)
+		{
+			ctx.MoveTo(X[0], Y[0]);
+			ctx.Stroke();
+
+			if (X.Count == 1) {
+				ctx.Arc(X[0], Y[0], ctx.LineWidth/2, 0, 2*3.1415);
+				ctx.Fill();
+			} else {
+				for (int i = 1; i < X.Count; i++) {
+					ctx.LineTo(X[i], Y[i]);
+				}
+
+				ctx.Stroke();
+			}
+		}
+	}
+}
+
