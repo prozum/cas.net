85c13d861dee9d2b11e20c598a867bb540fcf099
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-03-06 01:27:39 +0100

    Move to new system.... Pushing random stuff #2

---
+++ Samples/FormulaTest/FormulaTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//using Formula;
+using SymbolicMath;
+
+namespace FormulaTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Expression exp  = Expression.Parse("x+y");
+			Symbol x = new Symbol("x");
+			Symbol y = new Symbol("y");
+
+			var exp = x + y + 300/(x-100);
+
+			Console.WriteLine(exp.Repr);
+
+		}
+	}
+}
