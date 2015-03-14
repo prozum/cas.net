e32c9adb8dfd97a452f5164d99820a71cb2690a4
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-03-14 18:05:54 +0100

    sln: reorganize again + add libast

---
+++ Libraries/LibAst/Expression.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Ast
+{
+	public class Expression {}
+}
---
+++ Samples/AstParse/AstParse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Ast;
+
+class AstParse
+{
+	public static void Main (string[] args)
+	{
+		var exp = new Expression();
+	}
+}
+
