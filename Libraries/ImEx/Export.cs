675abe034d5c0fc10a19b30149233c5e8eb544f5
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-03-16 21:09:10 +0100

    Added Import/export lib * Can save objects to file.

---
+++ Libraries/ImEx/Export.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ImEx
+{
+	public static class Export
+	{
+		public static void WriteToCasFile (Object ExportObject, string FileName, string FileDestination)
+		{
+			string JsonFile = JsonConvert.SerializeObject (ExportObject);
+
+			JsonSerializer serializer = new JsonSerializer ();
+			serializer.NullValueHandling = NullValueHandling.Ignore;
+
+			File.WriteAllText (FileName + ".cas", JsonFile);
+
+			#if DEBUG
+			Console.WriteLine ("Writing to file \"" + FileName + ".cas\": " + JsonFile);
+			#endif
+		}
+	}
+}
+
