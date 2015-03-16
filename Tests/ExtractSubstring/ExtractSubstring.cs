dcd341bb15b62a51d510c08f03c52e7606f2728a
nimoov <>
2015-03-16 11:26:56 +0100

    Add ExtractSubstring test

---
+++ Tests/ExtractSubstring/ExtractSubstring.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using Ast;
+
+namespace ExtractSubstring
+{
+	[TestFixture]
+	public class ExtractSubstringTest
+	{
+		[Test]
+		public void ExtractSubstring()
+		{
+			var testString = "(x*y)*f(10-x)";
+
+			var testEnum = testString.GetEnumerator ();
+
+			//testEnum.MoveNext ();
+
+			var res = Ast.Parser.ExtractSubstring (testEnum, 'v');
+
+			Assert.AreEqual ("x*y", res);
+		}
+	}
+}
+
