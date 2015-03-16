367e5f21d25b39d14d20de9d68c0e43109c0d038
reindeernix <reindeernix@prozum.dk>
2015-03-16 21:23:56 +0100

    progressed on division operator and numers

--- Libraries/Ast/Operator.cs
+++ Libraries/Ast/Operator.cs
@@ -51,14 +51,50 @@ namespace Ast
 
 	public class Div : Operator
 	{
-		public Div(Expression a, Expression b)
-		{
 
-		}
-
+		/* fix errors */
 		public override Expression Evaluate (Expression a, Expression b)
 		{
+			if (a is Integer && b is Integer) {
+				return new Rational(a, b);
+			}
 
+			if (a is Integer && b is Rational) {
+				return new Div().Evaluate(new Rational(((Integer)a).value, 1), b);
+			}
+
+			if (a is Rational && b is Integer) {
+				return new Div().Evaluate(new Rational(b, ((Integer)b).value, 1));
+			}
+
+			if (a is Rational && b is Rational) {
+				return new Mul(new Rational(((Rational)a).numerator.value, ((Rational)a).denominator.value), b);
+			}
+
+			if (a is Integer && b is Irrational)
+			{
+				return new Irrational(((Integer)a).value / ((Irrational)b).value);
+			}
+
+			if (a is Irrational && b is Integer)
+			{
+				return new Irrational(((Irrational)a).value / ((Integer)b).value);
+			}
+
+			if (a is Irrational && b is Irrational)
+			{
+				return new Irrational(((Irrational)a).value / ((Irrational)b).value);
+			}
+
+			if (a is Irrational && b is Rational)
+			{
+				return new Irrational(((Irrational)a).value / ((Rational)b).value);
+			}
+
+			if (a is Rational && b is Irrational)
+			{
+				return new Irrational(((Rational)a).value / ((Irrational)b).value);
+			}
 		}
 	}
 
