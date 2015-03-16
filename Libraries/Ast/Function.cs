aa729f58c15eb1d353607a6c861661da3be636d4
reindeernix <reindeernix@prozum.dk>
2015-03-16 11:13:50 +0100

    added operators and trig functions

--- Libraries/Ast/Function.cs
+++ Libraries/Ast/Function.cs
@@ -1,9 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ast
 {
 	public abstract class Function  : Expression
 	{
+		public List<Expression> arglist = new List<Expression> ();
+
+		protected Function(params Expression[] args)
+		{
+			foreach (var item in args) {
+				arglist.Add (item);
+			}
+		}
+	}
+
+	public class Cos : Function
+	{
+		public Cos(params Expression[] args) : base(args)
+		{
+
+		}
+	}
+
+	public class Sin : Function
+	{
+		public Sin(params Expression[] args) : base(args)
+		{
+
+		}
+	}
+
+	public class Tan : Function
+	{
+		public Tan(params Expression[] args) : base(args)
+		{
+
+		}
+	}
+
+	public class ACos : Function
+	{
+		public ACos(params Expression[] args) : base(args)
+		{
+
+		}
+	}
+
+	public class ASin : Function
+	{
+		public ASin(params Expression[] args) : base(args)
+		{
+
+		}
+	}
+
+	public class ATan: Function
+	{
+		public ATan(params Expression[] args) : base(args)
+		{
+
+		}
 	}
 }
 
--- Libraries/Ast/Operator.cs
+++ Libraries/Ast/Operator.cs
@@ -2,10 +2,53 @@
 
 namespace Ast
 {
-	public class Operator
+	public abstract class Operator
 	{
-		public Operator ()
+		public abstract Expression Evaluate(Expression a, Expression b);
+	}
+
+	public class Add : Operator
+	{
+		public Add()
 		{
+
+		}
+
+		public override Expression Evaluate (Expression a, Expression b)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+
+	public class Sub : Operator
+	{
+		public override Expression Evaluate (Expression a, Expression b)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+
+	public class Mul : Operator
+	{
+		public override Expression Evaluate (Expression a, Expression b)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+
+	public class Div : Operator
+	{
+		public override Expression Evaluate (Expression a, Expression b)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+
+	public class Exp : Operator
+	{
+		public override Expression Evaluate (Expression a, Expression b)
+		{
+			throw new NotImplementedException ();
 		}
 	}
 }
