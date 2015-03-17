e2c35362605eccbef16230c16250e25817ec7fb5
BlizzardofStone <contesta_john@outlook.com>
2015-03-17 10:09:05 +0100

    ø

--- Samples/Triangle/Calc.cs
+++ Samples/Triangle/Calc.cs
@@ -21,6 +21,25 @@ namespace Geometry
 				return 0;
 			}
 		}
+		// Trigonometri, find vinkler ud fra sidelængder
+		public static double Trigono(char whichSide, double a, double b, double c) {
+			if (whichSide == 'A') {
+				if (a != 0 && c != 0) {
+					return Math.Asin(Math.Sin (a / c));
+				} else if (b != 0 && c != 0) {
+					return Math.Cosh(Math.Cos (b / c));
+				} else {
+					return 0;
+				}
+
+			} else if (whichSide == 'B') {
+				//hyp/kateta
+				return 0;
+			} else if (whichSide == 'C') {
+				return 0;
+			} else {
+				return 0;
+			}
+		}
 	}
 }
-
--- Samples/Triangle/Program.cs
+++ Samples/Triangle/Program.cs
@@ -6,8 +6,11 @@ namespace Geometry
 	{
 		public static void Main (string[] args)
 		{
-			//Areal beregning vha. pythagoras
-			Console.WriteLine ("{0}", Triangle.Pythagoras (2, 2, 0));
+			//Længde beregning vha. pythagoras
+			Console.WriteLine ("Længde af hypotenuse: {0}", Triangle.Pythagoras (2, 2, 0));
+			Console.WriteLine ("A^b_C");
+
+			Console.WriteLine ("Vinkel A: {0}", Triangle.Trigono ('A', 0, 3, 5));
 
 
 		}
