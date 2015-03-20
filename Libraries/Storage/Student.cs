a22e0150604ac1799755d23bffdd6be551047221
reindeernix <reindeernix@prozum.dk>
2015-03-20 16:59:17 +0100

    started working on Storage system for tasks

---
+++ Libraries/Storage/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Storage
+{
+	public class Student : IUser
+	{
+		public Student ()
+		{
+
+		}
+
+		public void GetTask()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PushTask()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ Libraries/Storage/Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Storage
+{
+	public class Teacher : IUser
+	{
+		public Teacher ()
+		{
+
+		}
+
+		public void AddStudent()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void GetTask()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PushTask()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
