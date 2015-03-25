9a52b0640f3b8e432c1a13768a9cb75d740eef3c
karlsmart11 <the.mark.lauridsen@gmail.com>
2015-03-25 13:03:34 +0100

    taskgen changes

--- Samples/TaskGen/Program.cs
+++ Samples/TaskGen/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TaskGen
 {
     class MainClass
     {
-        public static void Main (string[] args)
+        static void MakeTask ()
         {
-            Console.WriteLine ("Hello World!");
+            int varMin = 1;
+            int varMax = 10;
+            int varNum = 4;
+            int opsNum = 2;
+
+            List<string> Operators = new List<string> ();
+            List<int> Numbers = new List<int> ();
+            Random r = new Random ();
+
+            switch (opsNum) {
+            case 1:
+                Operators.Add ("+");
+                break;
+            case 2:
+                Operators.Add ("-");
+                break;
+            case 4: 
+                Operators.Add ("*");
+                break;
+            case 8: 
+                Operators.Add ("/");
+                break;
+            }
+
+            for (int i = 0; i < varNum; i++) {
+                Numbers.Add (r.Next (varMin, varMax));
+            }
+    
+            Console.Write (Numbers[0]);
+            for (int i = 0; i < varNum-1; i++) {
+                Console.Write (Operators[i]);
+                Console.Write (Numbers[i+1]);
+            }
+
+        }
+
+        public static void Main ()
+        {
+            MakeTask ();
         }
     }
 }
