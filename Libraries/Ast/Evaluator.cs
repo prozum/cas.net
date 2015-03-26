a557c756f0e0d59ad9d57ddfd6d6d519b2d92c6b
nimoov <nimoov@prozum.dk>
2015-03-26 11:34:28 +0100

    Ast: Add Evaluator class

---
+++ Libraries/Ast/Evaluator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ast
+{
+	public class Evaluator
+	{
+		public Dictionary<string,Expression> definitions = new Dictionary<string,Expression>();
+
+		public Evaluator ()
+		{
+		}
+	}
+}
+
