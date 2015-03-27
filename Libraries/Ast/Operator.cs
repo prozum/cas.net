ae99d5071b7685473854adec1d0a8ac67b0a2622
Hejsil <rainbowhejsil@gmail.com>
2015-03-27 13:44:55 +0100

    Change the codingstyle of Operator.cs to be more consistent

--- Libraries/Ast/Operator.cs
+++ Libraries/Ast/Operator.cs
@@ -38,14 +38,13 @@ namespace Ast
 
         public override string ToString()
         {
-            if (parent == null || priority >= parent.priority ) {
-
+            if (parent == null || priority >= parent.priority ) 
+            {
                 return left.ToString () + symbol + right.ToString ();
-			
-            } else {
-
+            } 
+            else 
+            {
                 return '(' + left.ToString () + symbol + right.ToString () + ')';
-
             }
         }
 
@@ -98,6 +97,7 @@ namespace Ast
             {
                 var leftNumerator = new Integer((left as Rational).numerator.value * (right as Rational).denominator.value);
                 var rightNumerator = new Integer((right as Rational).numerator.value * (left as Rational).denominator.value);
+
                 return new Rational(new Add(leftNumerator, rightNumerator).Evaluate() as Integer,
                                     new Mul((right as Rational).denominator, (left as Rational).denominator).Evaluate() as Integer);
             }
@@ -142,15 +142,18 @@ namespace Ast
 
 		public override Expression Evaluate ()
 		{
-			if (left is Integer && right is Integer) {
+			if (left is Integer && right is Integer) 
+            {
 				return new Integer((left as Integer).value - (right as Integer).value);
 			}
 
-			if (left is Integer && right is Rational) {
+			if (left is Integer && right is Rational) 
+            {
                 return new Sub(new Rational((left as Integer), new Integer(1)), right).Evaluate();
 			}
 
-			if (left is Rational && right is Integer) {
+			if (left is Rational && right is Integer) 
+            {
                 return new Sub(left, new Rational((right as Integer), new Integer(1))).Evaluate();
 			}
 
@@ -159,27 +162,33 @@ namespace Ast
             {
                 var leftNumerator = new Integer((left as Rational).numerator.value * (right as Rational).denominator.value);
                 var rightNumerator = new Integer((right as Rational).numerator.value * (left as Rational).denominator.value);
+
                 return new Rational(new Sub(leftNumerator, rightNumerator).Evaluate() as Integer,
                                     new Mul((right as Rational).denominator, (left as Rational).denominator).Evaluate() as Integer);
 			}
 
-			if (left is Integer && right is Irrational) {
+			if (left is Integer && right is Irrational) 
+            {
 				return new Irrational((left as Integer).value - (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Integer) {
+			if (left is Irrational && right is Integer) 
+            {
 				return new Irrational((left as Irrational).value - (right as Integer).value);
 			}
 
-			if (left is Irrational && right is Irrational) {
+			if (left is Irrational && right is Irrational) 
+            {
 				return new Irrational((left as Irrational).value - (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Rational) {
+			if (left is Irrational && right is Rational) 
+            {
 				return new Irrational((left as Irrational).value - (right as Rational).value.value);
 			}
 
-			if (left is Rational && right is Irrational) {
+			if (left is Rational && right is Irrational) 
+            {
 				return new Irrational((left as Rational).value.value - (right as Irrational).value);
 			}
 
@@ -198,40 +207,49 @@ namespace Ast
 
 		public override Expression Evaluate ()
 		{
-			if (left is Integer && right is Integer) {
+			if (left is Integer && right is Integer) 
+            {
 				return new Integer((left as Integer).value * (right as Integer).value);
 			}
 
-			if (left is Integer && right is Rational) {
+			if (left is Integer && right is Rational) 
+            {
                 return new Mul(new Rational((left as Integer), new Integer(1)), right).Evaluate();
 			}
 
-			if (left is Rational && right is Integer) {
+			if (left is Rational && right is Integer) 
+            {
                 return new Mul(left, new Rational((right as Integer), new Integer(1))).Evaluate();
 			}
 
-			if (left is Rational && right is Rational) {
+			if (left is Rational && right is Rational) 
+            {
                 return new Rational(new Mul((left as Rational).numerator, (right as Rational).numerator).Evaluate() as Integer,
                                    new Mul((left as Rational).denominator, (right as Rational).denominator).Evaluate() as Integer);
 			}
 
-			if (left is Integer && right is Irrational) {
+			if (left is Integer && right is Irrational)
+            {
 				return new Irrational((left as Integer).value * (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Integer) {
+			if (left is Irrational && right is Integer) 
+            {
 				return new Irrational((left as Irrational).value * (right as Integer).value);
 			}
 
-			if (left is Irrational && right is Irrational) {
+			if (left is Irrational && right is Irrational) 
+            {
 				return new Irrational((left as Irrational).value * (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Rational) {
+			if (left is Irrational && right is Rational) 
+            {
 				return new Irrational((left as Irrational).value * (right as Rational).value.value);
 			}
 
-			if (left is Rational && right is Irrational) {
+			if (left is Rational && right is Irrational) 
+            {
 				return new Irrational((left as Rational).value.value * (right as Irrational).value);
 			}
 
@@ -251,39 +269,48 @@ namespace Ast
 		/* fix errors */
 		public override Expression Evaluate ()
 		{
-			if (left is Integer && right is Integer) {
+			if (left is Integer && right is Integer) 
+            {
 				return new Rational((left as Integer), (right as Integer));
 			}
 
-			if (left is Integer && right is Rational) {
+			if (left is Integer && right is Rational) 
+            {
                 return new Div(new Rational((left as Integer), new Integer(1)), right).Evaluate();
 			}
 
-			if (left is Rational && right is Integer) {
+			if (left is Rational && right is Integer) 
+            {
                 return new Div(left, new Rational((right as Integer), new Integer(1))).Evaluate();
 			}
 
-			if (left is Rational && right is Rational) {
+			if (left is Rational && right is Rational) 
+            {
                 return new Mul(new Rational((left as Rational).denominator, (left as Rational).numerator), right).Evaluate();
 			}
 
-			if (left is Integer && right is Irrational) {
+			if (left is Integer && right is Irrational) 
+            {
 				return new Irrational((left as Integer).value / (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Integer) {
+			if (left is Irrational && right is Integer) 
+            {
 				return new Irrational((left as Irrational).value / (right as Integer).value);
 			}
 
-			if (left is Irrational && right is Irrational) {
+			if (left is Irrational && right is Irrational) 
+            {
 				return new Irrational((left as Irrational).value / (right as Irrational).value);
 			}
 
-			if (left is Irrational && right is Rational) {
+			if (left is Irrational && right is Rational) 
+            {
 				return new Irrational((left as Irrational).value / (right as Rational).value.value);
 			}
 
-			if (left is Rational && right is Irrational) {
+			if (left is Rational && right is Irrational) 
+            {
 				return new Irrational((left as Rational).value.value / (right as Irrational).value);
 			}
 
@@ -302,7 +329,8 @@ namespace Ast
 
         public override Expression Evaluate()
         {
-			if (left is Integer && right is Integer) {
+			if (left is Integer && right is Integer) 
+            {
 				return new Integer( (int)Math.Pow((left as Integer).value, (right as Integer).value));
 			}
 
