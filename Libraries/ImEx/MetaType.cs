8a1a3dd5427a4617a3bd1e405af3be842bc1cad4
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-04-07 10:50:38 +0200

    "Fixed" metatype type

---
+++ Libraries/ImEx/MetaType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ImEx
+{
+    public class MetaType
+    {
+        public Type type;
+        public string serializedString;
+
+        public MetaType()
+        {
+        }
+
+        public MetaType(object o)
+        {
+            this.type = o.GetType();
+            this.serializedString = Export.Serialize(o);
+        }
+    }
+}
+
