0201dc2781e142812637ed6cba0d99e2682f707f
nimoov <nimoov@prozum.dk>
2015-04-12 20:00:05 +0200

    CAS.NET.Desktop: Add Definition TreeView

--- CAS.NET.Desktop/MainWindow.cs
+++ CAS.NET.Desktop/MainWindow.cs
@@ -8,7 +8,10 @@ public class MainWindow : Window
 
     Grid grid;
 
-    TextView textview;
+    ListStore defStore;
+    TreeView defTree;
+
+    TextView textView;
     TextBuffer buffer;
 
     Entry entry;
@@ -31,28 +34,63 @@ public class MainWindow : Window
         buffer.Insert(buffer.StartIter, input.ToString() + " => " + output.ToString() + "\n");
     }
 
+    public void CreateDefTree()
+    {
+        CellRenderer renderer;
+
+        defStore = new ListStore(typeof(string), typeof(string));
+        defTree = new TreeView(defStore);
+        defTree.Expand = true;
+
+        renderer = new CellRendererText();
+        defTree.AppendColumn("Name", renderer, "text",0);
+        renderer = new CellRendererText();
+        defTree.AppendColumn("Value", renderer, "text",1);
+    }
+
+    public void UpdateDefinitions()
+    {
+        defStore.Clear();
+
+        foreach (var def  in eval.variableDefinitions)
+        {
+            defStore.AppendValues(def.Key, def.Value.ToString());
+        }
+
+        foreach (var def  in eval.functionDefinitions)
+        {
+            defStore.AppendValues(def.Key, def.Value.ToString());
+        }
+    }
+
     public MainWindow() : base("MainWindow")
     {
         DeleteEvent += (o, a) => Application.Quit ();
+
+        eval = new Evaluator ();
 
         SetSizeRequest(500, 500);
 
         grid = new Grid ();
         Add (grid);
 
-        eval = new Evaluator ();
-
         entry = new Entry ();
         entry.Activated += (o, a) => EvaluateEntry ();
+        entry.Activated += (o, a) => UpdateDefinitions();
         grid.Attach (entry, 0, 0, 1, 1);
 
-        textview = new TextView();
-        textview.Expand = true;
-        textview.Editable = false;
+        textView = new TextView();
+        textView.Expand = true;
+        textView.Editable = false;
         var sw = new ScrolledWindow ();
-        sw.Add(textview);
+        sw.Add(textView);
         grid.Attach (sw, 0, 1, 1, 1);
-        buffer = textview.Buffer;
+        buffer = textView.Buffer;
+
+        CreateDefTree();
+        grid.Attach(defTree, 1, 0, 1, 2);
+
+
 
         ShowAll ();
     }
