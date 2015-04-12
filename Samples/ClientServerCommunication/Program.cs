b1e9c3262a852a1674c40d1f718f0d34cdcd10bb
Kasper Steensig <reindeernix@prozum.dk>
2015-04-12 23:54:39 +0200

    forgot to add Program.cs and cleaned database a bit

--- Samples/ClientServerCommunication/Program.cs
+++ Samples/ClientServerCommunication/Program.cs
@@ -8,32 +8,32 @@ namespace ClientServerCommunication
     {
         public static void Main(string[] args)
         {
-            Teacher teacher = new Teacher("kasp6378");
-            Student student = new Student("kasp1234", "9A2016");
+            Teacher teacher = new Teacher("kasp6378", args[0]);
+            Student student = new Student("kasp1234", args[0]);
 
             string file = "jsonfileextremeoverload";
             string fileName = "testfile";
 
             string checksum = Checksum.GetMd5Hash(file);
 
-            Console.WriteLine(teacher.AddAssignment(checksum, file, fileName, "9A2016", "kasp6378", args[0]));
+            Console.WriteLine(teacher.AddAssignment(checksum, file, fileName, "9A2016"));
             
-            string[] list = student.GetAssignmentList("kasp1234", args[0]);
+            string[] list = student.GetAssignmentList();
 
             foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
                      
-            Console.WriteLine(student.GetAssignment("testfile", "kasp1234", args[0]));
+            Console.WriteLine(student.GetAssignment("testfile"));
 
-            Console.WriteLine(student.AddCompleted("jsonfileextremeoverload_completed", "testfile", "kasp1234", args[0]));
+            Console.WriteLine(student.AddCompleted("jsonfileextremeoverload_completed", "testfile"));
 
-            Console.WriteLine(teacher.GetCompleted("testfile", "9A2016", "kasp6378", args[0]));
+            Console.WriteLine(teacher.GetCompleted("testfile", "9A2016"));
 
-            Console.WriteLine(teacher.AddFeedback("jsonfileextremeoverload_completed_feedback", "testfile", "9A2016", "kasp6378", args[0]));
+            Console.WriteLine(teacher.AddFeedback("jsonfileextremeoverload_completed_feedback", "testfile", "9A2016"));
 
-            Console.WriteLine(student.GetFeedback("testfile", "kasp1234", args[0]));
+            Console.WriteLine(student.GetFeedback("testfile"));
         }
     }
 }
