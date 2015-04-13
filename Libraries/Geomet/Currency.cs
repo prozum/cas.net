4bd03caabc17684b5109251fd21f721c5bcf6a10
BlizzardofStone <contesta_john@outlook.com>
2015-04-13 10:26:16 +0200

    Small

---
+++ Libraries/Geomet/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Geomet
+{
+    public class Currency
+    {
+        public static double danishToOther(double u_kurs, double money)
+        {
+            return (u_kurs/100)*money;
+        }
+        public static double otherToDanish(double radius)
+        {
+            return 4 / 3 * Math.PI * radius * radius * radius;
+        }
+    }
+}
+
