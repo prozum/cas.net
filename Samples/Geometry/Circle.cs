b536d07342361f9747aaf83dfd47e52f4476a1ab
BlizzardofStone <contesta_john@outlook.com>
2015-04-13 09:33:59 +0200

    Added circle calc.

---
+++ Samples/Geometry/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Circle
+    {
+        public static double area(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double circumference(double radius)
+        {
+            return 2*Math.PI * radius;
+        }
+
+    }
+}
+
