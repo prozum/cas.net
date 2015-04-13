8a81c9357a7e90a3440af4a3e1f5cc8705043675
reindeernix <reindeernix@prozum.dk>
2015-04-13 16:36:19 +0200

    changed mysql user

--- Tests/ClientServer.Tests/ClientServer/Test.cs
+++ Tests/ClientServer.Tests/ClientServer/Test.cs
@@ -11,7 +11,7 @@ namespace ClientServer
     public class Test
     {
 		const string host = "http://localhost:8080/";
-		const string database = @"server=localhost;userid=root;password=;database=mydb";
+		const string database = @"server=localhost;userid=travis2;database=mydb";
 
 		Teacher teacher = new Teacher("teacher", "passwd0");
 		Student student1 = new Student("student1", "passwd1");
