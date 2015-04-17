f160f25ab8e1518263f6974e91167b11c61c4b30
nimoov <nimoov@prozum.dk>
2015-04-17 16:44:02 +0200

    Libraries: Add new lib Tokenizer(Not implemented)

---
+++ Libraries/Tokenizer/Tokenizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ast
+{
+    public static class Tokenizer
+    {
+        public static List<Token> Tokenize(string str)
+        {
+            var tokens = new List<Token>();
+
+            throw new NotImplementedException();
+        }
+    }
+}
+
