2e612608502894b90eeb1b652102b4ca2409f54d
reindeernix <reindeernix@prozum.dk>
2015-04-20 11:40:36 +0200

    deleted ref

--- Libraries/DesktopUI/SettingsToolbar.cs
+++ Libraries/DesktopUI/SettingsToolbar.cs
@@ -15,7 +15,7 @@ namespace DesktopUI
 		List<Widget> widgets;
 		Window window;
 
-		public SettingsToolbar (ref string file, ref Grid grid, ref int GridNumber, ref List<MetaType> meta, ref List<Widget> widgets, ref Window window)
+		public SettingsToolbar (ref string file, ref Grid grid, ref int GridNumber, ref List<MetaType> meta, ref List<Widget> widgets, Window window)
 		{
 			this.file = file;
 			this.grid = grid;
@@ -91,28 +91,27 @@ namespace DesktopUI
 
 					break;
 				}
-			default:
-				break;
-
-				ClearWindow ();
-
-				meta.Clear ();
-				List<MetaType> mtlmt = new List<MetaType> ();
-				mtlmt = Import.DeserializeString<List<MetaType>> (file);
-				meta = mtlmt;
-				widgets.Clear ();
-
-				/*
-				RecreateListWidget ();
-
-				foreach (Widget item in widgets) {
-					grid.Attach (MovableWidget (item), 1, gridNumber, 1, 1);
-					gridNumber++;
-				}
-				*/
-
-				ShowAll ();
 			}
+
+			ClearWindow ();
+
+			meta.Clear ();
+			List<MetaType> mtlmt = new List<MetaType> ();
+			mtlmt = Import.DeserializeString<List<MetaType>> (file);
+			meta = mtlmt;
+			widgets.Clear ();
+
+			//RecreateListWidget ();
+
+			/*
+			foreach (Widget item in widgets)
+			{
+				grid.Attach (MovableWidget (item), 1, gridNumber, 1, 1);
+				gridNumber++;
+			}
+			*/
+
+			ShowAll ();
 		}
 
 		void SaveFile()
@@ -136,46 +135,46 @@ namespace DesktopUI
 			case PlatformID.Win32Windows:
 			case PlatformID.WinCE:
 			case PlatformID.Win32NT: // <- if one, this is the one we really need
+			{
+				var filechooser = new System.Windows.Forms.SaveFileDialog();
+
+				filechooser.InitialDirectory = "c:\\";
+				filechooser.Filter = "cas files (*.cas)|*.cas";
+				filechooser.FilterIndex = 2;
+				filechooser.RestoreDirectory = true;
+
+				if (filechooser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
 				{
-					var filechooser = new System.Windows.Forms.SaveFileDialog();
-
-					filechooser.InitialDirectory = "c:\\";
-					filechooser.Filter = "cas files (*.cas)|*.cas";
-					filechooser.FilterIndex = 2;
-					filechooser.RestoreDirectory = true;
-
-					if (filechooser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-					{
-						System.IO.File.WriteAllText(filechooser.FileName, file);
-					}
-
-					break;
+					System.IO.File.WriteAllText(filechooser.FileName, file);
 				}
+
+				break;
+			}
 			case PlatformID.Unix:
 			case PlatformID.MacOSX:
+			{
+				Object[] parameters = { "Cancel", ResponseType.Cancel, "Save", ResponseType.Accept };
+				FileChooserDialog filechooser = new FileChooserDialog("Save File...", window, FileChooserAction.Save, parameters);
+
+				filechooser.Filter = new FileFilter();
+				filechooser.Filter.AddPattern("*.cas");
+
+				if (filechooser.Run() == (int)ResponseType.Accept)
 				{
-					Object[] parameters = { "Cancel", ResponseType.Cancel, "Save", ResponseType.Accept };
-					FileChooserDialog filechooser = new FileChooserDialog("Save File...", window, FileChooserAction.Save, parameters);
-
-					filechooser.Filter = new FileFilter();
-					filechooser.Filter.AddPattern("*.cas");
-
-					if (filechooser.Run() == (int)ResponseType.Accept)
+					if (filechooser.Name.ToLower().EndsWith(".cas"))
 					{
-						if (filechooser.Name.ToLower().EndsWith(".cas"))
-						{
-							System.IO.File.WriteAllText(filechooser.Filename, file);
-						}
-						else
-						{
-							System.IO.File.WriteAllText(filechooser.Filename + ".cas", file);
-						}
+						System.IO.File.WriteAllText(filechooser.Filename, file);
 					}
-
-					filechooser.Destroy();
-
-					break;
+					else
+					{
+						System.IO.File.WriteAllText(filechooser.Filename + ".cas", file);
+					}
 				}
+
+				filechooser.Destroy();
+
+				break;
+			}
 			default:
 				{
 					break;
