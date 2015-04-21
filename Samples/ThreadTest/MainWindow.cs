b2165c1b8ba41dad085d2bdd77d8e30bc213e8a3
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-04-21 20:44:57 +0200

    [Sample/ThreadTest] Small changes The textview now increment by one on button press, and request update from thread.

--- Samples/ThreadTest/MainWindow.cs
+++ Samples/ThreadTest/MainWindow.cs
@@ -7,6 +7,7 @@ namespace ThreadTest
     public class MyClass : Window
     {
         static Label l;
+        static int numClick = 0;
 
         public static void Main()
         {
@@ -33,14 +34,15 @@ namespace ThreadTest
 
             Grid grid = new Grid();
 
-            l = new Label("Test");
+            l = new Label(numClick.ToString());
             grid.Attach(l, 1, 1, 1, 1);
 
             Button b = new Button("ClickMe!");
             grid.Attach(b, 1, 2, 1, 1);
             b.Clicked += delegate
             {
-                l.Text += " Clicked";
+                numClick++;
+                l.Text = numClick.ToString();
                 l.QueueDraw();
             };
 
