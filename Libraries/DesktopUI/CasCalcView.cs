11a57e75143d3a663b7ba03b6b517ede4697a194
Kasper Steensig <reindeernix@prozum.dk>
2015-04-22 19:11:15 +0200

    added CasCalcView

---
+++ Libraries/DesktopUI/CasCalcView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ast;
+using Gtk;
+
+namespace DesktopUI
+{
+	public class CasCalcView : CasTextView
+	{
+		public CasCalcView(TextViewList parent, string serializedString, bool teacherCanEdit) :
+		base(parent, serializedString, teacherCanEdit)
+		{
+
+		}
+
+		public void Evaluate()
+		{
+			string expression = String.Empty;
+			string buffer = Buffer.ToString();
+			int i = 0;
+
+			while (buffer[i] != '\n')
+			{
+				expression += buffer[i];
+			}
+
+			Evaluator Eval = new Evaluator();
+			EvalData Data = Eval.Evaluation(expression);
+
+			Buffer.Clear();
+			TextIter textIter = Buffer.StartIter;
+			byte[] EvaluationBytes = Convert.FromBase64String(expression += '\n' + Data.ToString());
+			Buffer.Deserialize(Buffer, Buffer.RegisterDeserializeTagset(null), ref textIter, EvaluationBytes, (ulong)EvaluationBytes.Length);
+		}
+	}
+}
