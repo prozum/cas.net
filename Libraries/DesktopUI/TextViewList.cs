0166eaf3dc85321823ea7de20e044977cfa3813a
Kasper Steensig <reindeernix@prozum.dk>
2015-04-22 13:08:02 +0200

    mother of god it works

--- Libraries/DesktopUI/TextViewList.cs
+++ Libraries/DesktopUI/TextViewList.cs
@@ -7,15 +7,28 @@ namespace DesktopUI
 	public class TextViewList : Grid
 	{
 		List<MovableCasTextView> castextviews = new List<MovableCasTextView>();
+		Button AddNewMovableTextView = new Button("+");
 
 		public TextViewList() : base()
 		{
-			
+			AddNewMovableTextView.Clicked += delegate
+				{
+					Insert("", false);
+				};
 		}
 
 		public void Insert(string serializedString, bool teacherCanEdit)
 		{
-			Attach(new MovableCasTextView(serializedString, teacherCanEdit), 1, Children.Length, 1, 1);
+			castextviews.Add(new MovableCasTextView(serializedString, teacherCanEdit));
+
+			Clear();
+
+			foreach (Widget widget in castextviews)
+			{
+				Attach(widget, 1, Children.Length, 1, 1);
+			}
+
+			Attach(AddNewMovableTextView, 1, Children.Length, 1, 1);
 			ShowAll();
 		}
 
