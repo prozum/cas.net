bce0c378c8ee461ccbaa2faf378904d7b6b3d580
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-04-22 15:09:00 +0200

    [Lib/DesktopUI] Server menu now shows menus relevant to the user that is currently logged in.

--- Libraries/DesktopUI/TeacherGetAssignmentListMenuItem.cs
+++ Libraries/DesktopUI/TeacherGetAssignmentListMenuItem.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Gtk;
 
 namespace DesktopUI
 {
-    public class TeacherGetAssignmentListMenuItem
+    public class TeacherGetAssignmentListMenuItem : MenuItem
     {
         public TeacherGetAssignmentListMenuItem()
+            : base("Get List Of Assignments")
         {
         }
     }
