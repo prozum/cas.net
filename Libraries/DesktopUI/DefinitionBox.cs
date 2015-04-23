c62291fd7d09a8141ae945786b7806b48e72b47c
Kasper Steensig <reindeernix@dinkpad>
2015-04-23 10:23:32 +0200

    added initial work for DefinitionBox

---
+++ Libraries/DesktopUI/DefinitionBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ast;
+using Gtk;
+
+namespace DesktopUI
+{
+	public class DefinitionBox : TextView
+	{
+		TextViewList textviews;
+
+		public DefinitionBox (TextViewList textviews) : base()
+		{
+			this.textviews = textviews;
+		}
+	}
+}
+
