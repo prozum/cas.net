038b61bb8b01853be70d16a28b5d9cb45943d1e5
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-04-23 15:17:48 +0200

    [Lib/DesktopUI] Work on user and server comm

--- Libraries/Account/Student.cs
+++ Libraries/Account/Student.cs
@@ -8,105 +8,107 @@ namespace Account
 {
     public class Student
     {
-		private readonly string host;
-		private WebClient client;
+        private readonly string host;
+        private WebClient client;
 
-		public Student (string username, string password, string host)
+        // http://localhost/:8080
+
+        public Student(string username, string password, string host)
         {
-			this.host = host;
-			client = new WebClient();
-			client.Encoding = System.Text.Encoding.UTF8;
-			client.Credentials = new NetworkCredential(username, password);
+            this.host = host;
+            client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            client.Credentials = new NetworkCredential(username, password);
         }
 
         public string AddCompleted(string file, string filename)
         {
-			client.Headers.Add ("Checksum", Checksum.GetMd5Hash (file));
-			client.Headers.Add ("Filename", filename);
-			client.Headers.Add ("File", file);
+            client.Headers.Add("Checksum", Checksum.GetMd5Hash(file));
+            client.Headers.Add("Filename", filename);
+            client.Headers.Add("File", file);
 
-			string response = client.UploadString(host, "AddCompleted");
+            string response = client.UploadString(host, "AddCompleted");
 
-			client.Headers.Clear();
+            client.Headers.Clear();
 
-			return response;
+            return response;
         }
 
         public string[] GetAssignmentList()
         {
-			string response = client.UploadString(host, "StudentGetAssignmentList");
+            string response = client.UploadString(host, "StudentGetAssignmentList");
 
-			client.Headers.Clear();
+            client.Headers.Clear();
 
-			if (response == "Success")
-			{
-				string[] filelist = new string[client.ResponseHeaders.Count/2];
-				string[] checksumlist = new string[client.ResponseHeaders.Count/2];
+            if (response == "Success")
+            {
+                string[] filelist = new string[client.ResponseHeaders.Count / 2];
+                string[] checksumlist = new string[client.ResponseHeaders.Count / 2];
 
-				for (int i = 0; i < client.ResponseHeaders.Count/2; i++)
-				{
-					filelist[i] = client.ResponseHeaders["File" + i.ToString()];
-					checksumlist[i] = client.ResponseHeaders["Checksum" + i.ToString()];
+                for (int i = 0; i < client.ResponseHeaders.Count / 2; i++)
+                {
+                    filelist[i] = client.ResponseHeaders["File" + i.ToString()];
+                    checksumlist[i] = client.ResponseHeaders["Checksum" + i.ToString()];
 
-					if (Checksum.GetMd5Hash(filelist[i]) != checksumlist[i])
-					{
-						return this.GetAssignmentList();
-					}
-				}
-			}
+                    if (Checksum.GetMd5Hash(filelist[i]) != checksumlist[i])
+                    {
+                        return this.GetAssignmentList();
+                    }
+                }
+            }
 
-			return null;
+            return null;
         }
 
         public string GetAssignment(string filename)
         {
-			client.Headers.Add ("Filename", filename);
+            client.Headers.Add("Filename", filename);
 
-			string response = client.UploadString(host, "GetAssignment");
+            string response = client.UploadString(host, "GetAssignment");
 
-			client.Headers.Clear();
+            client.Headers.Clear();
 
-			if (response == "Success")
-			{
-				string file = client.ResponseHeaders["File"];
-				string checksum = client.ResponseHeaders["Checksum"];
+            if (response == "Success")
+            {
+                string file = client.ResponseHeaders["File"];
+                string checksum = client.ResponseHeaders["Checksum"];
 
-				if (checksum == Checksum.GetMd5Hash(file))
-				{
-					return file;
-				}
-				else
-				{
-					return this.GetAssignment(filename);
-				}
-			}
-			Console.WriteLine("noice");
-			return null;
+                if (checksum == Checksum.GetMd5Hash(file))
+                {
+                    return file;
+                }
+                else
+                {
+                    return this.GetAssignment(filename);
+                }
+            }
+            Console.WriteLine("noice");
+            return null;
         }
 
-		public string GetFeedback(string filename)
+        public string GetFeedback(string filename)
         {
-			client.Headers.Add ("Filename", filename);
-			string response = client.UploadString(host, "GetFeedback");
+            client.Headers.Add("Filename", filename);
+            string response = client.UploadString(host, "GetFeedback");
 
-			client.Headers.Clear();
+            client.Headers.Clear();
 
-			if (response == "Success")
-			{
-				string file = client.ResponseHeaders["File"];
-				string checksum = client.ResponseHeaders["Checksum"];
+            if (response == "Success")
+            {
+                string file = client.ResponseHeaders["File"];
+                string checksum = client.ResponseHeaders["Checksum"];
 
-				if (checksum == Checksum.GetMd5Hash(file))
-				{
-					return file;
-				}
-				else
-				{
-					return this.GetFeedback(filename);
-				}
-			}
+                if (checksum == Checksum.GetMd5Hash(file))
+                {
+                    return file;
+                }
+                else
+                {
+                    return this.GetFeedback(filename);
+                }
+            }
 
-			return null;
+            return null;
         }
     }
 }
