74f61fe9777d91c06666238be7cfc924e83aa8bf
nimoov <nimoov@prozum.dk>
2015-04-25 10:30:45 +0200

    [Ast] Remove unused code #2

--- Libraries/Ast/Evaluator.cs
+++ Libraries/Ast/Evaluator.cs
@@ -15,41 +15,9 @@ namespace Ast
 
         public EvalData Evaluation(string inputString)
         {
-            var res = Parser.Parse(inputString, scope);
-
-            var exp = res.Evaluate();
-
-            if (exp is Assign)
-            {
-                if ((exp as Assign).Left is UsrFunc)
-                {
-                    return AssignFunction((exp as Assign).Left as UsrFunc, (exp as Assign).Right);
-                }
-                if ((exp as Assign).Left is Symbol)
-                {
-                    return AssignSymbol((exp as Assign).Left as Symbol, (exp as Assign).Right);
-                }
-                else
-                {
-                    return new MsgData(MsgType.Error, "Evaluator> Left expression is not a symbol or function");
-                }
-            }
-
-            if (exp is Plot)
-            {
-                if ((exp as Plot).isArgsValid())
-                    return new PlotData((Plot) exp);
-
-                exp = new ArgError((Plot)exp);
-            }
-            else if (exp is Simplify || exp is Expand)
-            {
-                exp = exp.Evaluate();
-            }
-            else
-            {
-                exp = exp.Simplify().Evaluate();
-            }
+            scope.statements.Clear();
+            Parser.Parse(inputString, scope);
+            var exp = scope.Evaluate();
 
             if (exp is Error)
             {
@@ -62,46 +30,6 @@ namespace Ast
             }
 
             return new MsgData(MsgType.Print, exp.ToString());
-        }
-
-        public EvalData AssignFunction(UsrFunc func, Expression expr)
-        {
-            if (expr.ContainsVariable(func))
-            {
-                return new MsgData(MsgType.Error, "Evaluator> Can't define function as it self");
-            }
-
-            func.expr = expr;
-            func.scope = new Scope(scope);
-            scope.SetVar(func.identifier, func);
-
-            foreach (var exp in func.args)
-            {
-                if (exp is Symbol)
-                {
-                    var sym = (Symbol)exp;
-                    sym.scope = func.scope;
-                    func.scope.SetVar(sym.identifier, sym);
-                }
-                else
-                {
-                    return new MsgData(MsgType.Error, "Evaluator> One arg in the function is not a symbol");
-                }
-            }
-
-            return new MsgData(MsgType.Info, "Evaluator> Function '" + func.ToString() + "' defined");
-        }
-
-        public EvalData AssignSymbol(Symbol sym, Expression exp)
-        {
-            if (exp.ContainsVariable(sym))
-            {
-                return new MsgData(MsgType.Error, "Evaluator> Can't define symbol as it self");
-            }
-
-            scope.SetVar(sym.identifier, exp);
-
-            return new MsgData(MsgType.Info, "Evaluator> Variable '" + sym.ToString() + "' defined");
         }
     }
 }
