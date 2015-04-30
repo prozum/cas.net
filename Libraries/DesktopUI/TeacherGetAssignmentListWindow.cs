35e6dc0a8ae24bb5ca193e801f71439127a44e55
Kasper Steensig <reindeernix@prozum.dk>
2015-04-30 10:47:11 +0200

    [DesktopUI] working on an overhaul for the teacher control system

--- Libraries/DesktopUI/TeacherGetAssignmentListWindow.cs
+++ Libraries/DesktopUI/TeacherGetAssignmentListWindow.cs
@@ -23,43 +23,14 @@ namespace DesktopUI
             {
                 if (!string.IsNullOrEmpty(item))
                 {
-                    Button button = new Button(item);
-                    button.Clicked += delegate
-                        {
-                            string assignment = this.user.teacher.GetAssignment(item);
+                    Label label = new Label(item);
 
-                            List<MetaType> metaTypeList = new List<MetaType>();
+                    Button GetCompleted = new Button("Get Completed Assignments");
+                    GetCompleted.Clicked += (sender, e) => Destroy();
 
-                            metaTypeList = ImEx.Import.DeserializeString<List<MetaType>>(assignment);
+                    HBox hbox = new HBox(false, 2);
 
-                            this.textviews.castextviews.Clear();
-
-                            foreach (var metaItem in metaTypeList)
-                            {
-                                if (metaItem.type == typeof(MovableCasCalcView))
-                                {
-                                    Evaluator Eval = new Evaluator();
-                                    MovableCasCalcView movableCasCalcView = new MovableCasCalcView(Eval);
-                                    movableCasCalcView.calcview.input.Text = metaItem.metastring;
-                                    this.textviews.castextviews.Add(movableCasCalcView);
-                                }
-                                else if (metaItem.type == typeof(MovableCasTextView))
-                                {
-                                    MovableCasTextView movableCasTextView = new MovableCasTextView(metaItem.metastring, true);
-                                    this.textviews.castextviews.Add(movableCasTextView);
-                                }
-                            }
-
-                            this.textviews.castextviews.Reverse();
-
-                            this.textviews.Clear();
-                            this.textviews.Redraw();
-                            this.textviews.Reevaluate();
-                            this.textviews.ShowAll();
-
-                            Destroy();
-                        };
-                    vbox.Add(button);
+                    vbox.Add(hbox);
                 }
             }
 
@@ -72,4 +43,3 @@ namespace DesktopUI
         }
     }
 }
-
