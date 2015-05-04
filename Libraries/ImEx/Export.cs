4870f8697e7a5d954ad7f0201af005ed09edd307
BlizzardofStone <contesta_john@outlook.com>
2015-05-04 11:39:28 +0200

    Identation

--- Libraries/ImEx/Export.cs
+++ Libraries/ImEx/Export.cs
@@ -15,7 +15,7 @@ namespace ImEx
         public static void WriteToCasFile (string exportString, string fileName, string fileDestination)
         {    
 			File.WriteAllText (fileDestination + fileName+"_temp" + ".cas", exportString);
-			CompressToFile (fileName);
+		    CompressToFile (fileName);
         }
 
         // Writes a string to disk
@@ -23,35 +23,34 @@ namespace ImEx
         public static void WriteToCasFile (string exportString, string file)
         {
             File.WriteAllText (file+"_temp", exportString);
-			CompressToFile (file);
+		    CompressToFile (file);
         }
 
         // Writes an object to disk
         public static void WriteToCasFile (object exportObject, string file)
         {
             string serializedObject = Export.Serialize (exportObject);
-			File.WriteAllText (file+"_temp", serializedObject);
-			CompressToFile (file);
+		    File.WriteAllText (file+"_temp", serializedObject);
+		    CompressToFile (file);
         }
 
-		public static void CompressToFile(string fileName) {
-			FileStream sourceFileStream = File.OpenRead(fileName+"_temp");
-			FileStream destFileStream = File.Create(fileName);
+        public static void CompressToFile(string fileName) {
+		    FileStream sourceFileStream = File.OpenRead(fileName+"_temp");
+		    FileStream destFileStream = File.Create(fileName);
+		    
+		    GZipStream compressingStream = new GZipStream(destFileStream, 
+		        CompressionMode.Compress);
 
-			GZipStream compressingStream = new GZipStream(destFileStream, 
-				CompressionMode.Compress);
-
-			byte[] bytes = new byte[2048];
-			int bytesRead;
-			while ((bytesRead = sourceFileStream.Read(bytes, 0, bytes.Length)) != 0)
-			{
-				compressingStream.Write(bytes, 0, bytesRead);
+		    byte[] bytes = new byte[2048];
+	        int bytesRead;
+			while ((bytesRead = sourceFileStream.Read (bytes, 0, bytes.Length)) != 0) {
+				compressingStream.Write (bytes, 0, bytesRead);
 			}
 
-			sourceFileStream.Close();
-			compressingStream.Close();
-			destFileStream.Close();
-			System.IO.File.Delete (fileName + "_temp");
+		    sourceFileStream.Close();
+		    compressingStream.Close();
+		    destFileStream.Close();
+		    System.IO.File.Delete (fileName + "_temp");
 		}
 
 
