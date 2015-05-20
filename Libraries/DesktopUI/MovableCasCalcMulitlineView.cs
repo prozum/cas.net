861937da14e8d0b47f29ae6c8a27e4d2b033f877
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-20 13:54:50 +0200

    [Lib/DesktopUI] Working cascalcmultiline widget * removed drawcanvas, can draw with calcview

---
+++ Libraries/DesktopUI/MovableCasCalcMulitlineView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ast;
+
+namespace DesktopUI
+{
+    public class MovableCasCalcMulitlineView : MovableCasTextView
+    {
+        public CasCalcMultilineView calcview;
+
+        public MovableCasCalcMulitlineView(Evaluator eval) : base("",false)
+        {
+            calcview = new CasCalcMultilineView(eval);
+
+            Remove(textview);
+            Attach(calcview, 1, 1, 1, 2);
+        }
+    }
+}
---
+++ Libraries/DesktopUI/MovableCasCalcMultilineToolButton.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gtk;
+using System.IO;
+using Gdk;
+
+namespace DesktopUI
+{
+    public class MovableCasCalcMultilineToolButton : ToolButton
+    {
+        TextViewList textviews;
+        static Image image = new Image();
+
+        public MovableCasCalcMultilineToolButton(ref TextViewList textviews)
+            : base(image, "Multiline CalcView")
+        {
+            this.textviews = textviews;
+
+            TooltipText = "Multiline CalcView";
+
+            SetIcon();
+
+            Clicked += delegate
+            {
+                OnActivated();
+            };
+        }
+
+        private void OnActivated()
+        {
+            textviews.InsertCalcMultilineView(-1);
+        }
+
+        // Sets the icon for the widget
+        void SetIcon()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            PlatformID pid = os.Platform;
+
+            switch (pid)
+            {
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                case PlatformID.Win32NT: // <- if one, this is the one we really need
+                    {
+                        byte[] buffer = File.ReadAllBytes("..\\..\\..\\Ressources\\Icons\\Gnome-accessories-calculator.png");
+                        Pixbuf pixbuf = new Pixbuf(buffer);
+                        pixbuf = pixbuf.ScaleSimple(25, 25, InterpType.Bilinear);
+                        image.Pixbuf = pixbuf;
+
+                        break;
+                    }
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                    {
+                        byte[] buffer = File.ReadAllBytes("../../../Ressources/Icons/Gnome-accessories-calculator.svg");
+                        Pixbuf pixbuf = new Pixbuf(buffer);
+                        pixbuf = pixbuf.ScaleSimple(25, 25, InterpType.Bilinear);
+                        image.Pixbuf = pixbuf;
+
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+    }
+}
