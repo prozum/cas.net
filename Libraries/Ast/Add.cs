9424ae1a224ff9ce399cf9c83cb8ad404ce1c785
Hejsil <rainbowhejsil@gmail.com>
2015-05-21 10:52:42 +0200

    [Ast] Add and BinaryOpeator comments

--- Libraries/Ast/Add.cs
+++ Libraries/Ast/Add.cs
@@ -2,9 +2,6 @@
 
 namespace Ast
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class Add : BinaryOperator, ISwappable, IInvertable
     {
         public override string Identifier { get { return "+"; } }
@@ -25,34 +22,42 @@ namespace Ast
 
         protected override Expression ReduceHelper(Expression left, Expression right)
         {
+            //When left is 0, return right. "0+x==x"
             if (left is Real && left.CompareTo(Constant.Zero))
             {
                 return right;
             }
+            //When right is 0, return left. "x+0==x"
             else if (right is Real && right.CompareTo(Constant.Zero))
             {
                 return left;
             }
+            //When both sides are real, add them together. "2+2==4"
             else if (left is Real && right is Real)
             {
                 return left + right;
             }
+            //When left is add, go into that add and check if right can be reduced with left's sides.
             else if (left is Add)
             {
                 return (left as Add).ReduceMultiAdd(right);
             }
+            //When right is add, go into that add and check if left can be reduced with right's sides.
             else if (right is Add)
             {
                 return (right as Add).ReduceMultiAdd(left);
             }
+            //When both are the same variable, and there exponent are the same. "x^2 + x^2 == 2x^2"
             else if ((left is Variable && right is Variable) && CompareVariables(left as Variable, right as Variable))
             {
                 return VariableOperation(left as Variable, right as Variable);
             }
+            //When the sides are the same. "(x*y)+(y*x)==2xy"
             else if (left.CompareTo(Right))
             {
                 return new Mul(new Integer(2), left);
             }
+            //Couldn't reduce
             else
             {
                 return new Add(left, right);
@@ -87,14 +92,17 @@ namespace Ast
             }
             else
             {
+                //When other is the same as left. ((x*y)+y)+(x*y)==2(x*y)+y
                 if (Left.CompareTo(other))
                 {
                     return new Add(new Mul(new Integer(2), other), Right);
                 }
+                //When other is the same as right. (y+(x*y))+(x*y)==y+2(x*y)
                 else if (Right.CompareTo(other))
                 {
                     return new Add(Left, new Mul(new Integer(2), other));
                 }
+                //Couldn't reduce
                 else
                 {
                     return new Add(this, other);
@@ -104,14 +112,17 @@ namespace Ast
 
         private Expression ReduceMultiAdd(Real other)
         {
+            //When left is real, add other to left. (5+x)+5==10+x
             if (Left is Real)
             {
                 return new Add(Left + other, Right);
             }
+            //When right is real, add other to right. (x+5)+5==x+10
             else if (Right is Real)
             {
                 return new Add(Left, Right + other);
             }
+            //Couldn't reduce
             else
             {
                 return new Add(this, other);
@@ -120,18 +131,22 @@ namespace Ast
 
         private Expression ReduceMultiAdd(Variable other)
         {
+            //When left and other are the same variable, and there exponent are the same. "(x^2+x)+x^2 == 2x^2+x"
             if (Left is Variable && CompareVariables(Left as Variable, other))
             {
                 return new Add(VariableOperation(Left as Variable, other), Right);
             }
+            //When right and other are the same variable, and there exponent are the same. "(x+x^2)+x^2 == x+2x^2"
             else if (Right is Variable && CompareVariables(Right as Variable, other))
             {
                 return new Add(Left, VariableOperation(Right as Variable, other));
             }
+            //When left is add, go into that add and check if other can be reduced with left's sides.
             else if (Left is Add)
             {
                 var res = new Add((Left as Add).ReduceMultiAdd(other), Right);
 
+                //If true then Couldn't reduce
                 if (res.ToString() == new Add(new Add(Left, other), Right).ToString())
                 {
                     res = new Add(this, other);
@@ -139,10 +154,12 @@ namespace Ast
 
                 return res;
             }
+            //When right is add, go into that add and check if other can be reduced with right's sides.
             else if (Right is Add)
             {
                 var res = new Add(Left, (Right as Add).ReduceMultiAdd(other));
 
+                //If true then Couldn't reduce
                 if (res.ToString() == new Add(Left, new Add(Right, other)).ToString())
                 {
                     res = new Add(this, other);
@@ -150,12 +167,14 @@ namespace Ast
 
                 return res;
             }
+            //Couldn't reduce
             else
             {
                 return new Add(this, other);
             }
         }
 
+        //Returns to Variables added together. 2x+3x == 5x
         private Expression VariableOperation(Variable left, Variable right)
         {
             var res = left.Clone();
@@ -167,6 +186,7 @@ namespace Ast
 
         internal override Expression CurrectOperator()
         {
+            //When right is negative, return a sub, with right as positive. 2+(-2)==2-2.
             if (Right is INegative && (Right as INegative).IsNegative())
             {
                 return new Sub(Left.CurrectOperator(), (Right as INegative).ToNegative());
@@ -180,33 +200,24 @@ namespace Ast
             return new Add(Left.Clone(), Right.Clone());
         }
 
-        /// 
-        /// summary>
-        /// 
-        /// </summary>
         public Expression InvertOn(Expression other)
         {
             return new Sub(other, Right);
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public BinaryOperator Swap()
         {
             return new Add(Right, Left);
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
         public BinaryOperator Transform()
         {
-
+            //When left is add, make right add instead. (x+y)+z==x+(y+z)
             if (Left is Add)
             {
                 return new Add((Left as Add).Left, new Add((Left as Add).Right, Right));
             }
+            //When right is add, make right add instead. x+(y+z)==(x+y)+z
             else if (Right is Add)
             {
                 return new Add(new Add(Left, (Right as Add).Left), (Right as Add).Right);
