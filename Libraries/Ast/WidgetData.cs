d32ffa3bb83ae11a8dff49738693fe4f498c3d42
Kasper Steensig <reindeernix@prozum.dk>
2015-05-22 17:37:08 +0200

    [Ast] Moved WidgetData

---
+++ Libraries/Ast/WidgetData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ast
+{
+    public class WidgetData : EvalData
+    {
+        public string widget;
+
+        public WidgetData(string widget)
+        {
+            this.widget = widget;
+        }
+
+        public override string ToString()
+        {
+            return widget;
+        }
+    }
+}
+
