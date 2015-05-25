c8ee87e33622ffb07615a7c31c2fa5eae7a2d296
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-25 09:35:26 +0200

    [Android] Added android to master, removed mobile

---
+++ CAS.NET.Android/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Ast;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CAS.NET.Android
+{
+	[Activity (Label = "CAS.NET.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		TextView textviewinput;
+		TextView textviewoutput;
+		Evaluator eval = new Evaluator();
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			textviewinput = FindViewById<TextView> (Resource.Id.input);
+			textviewoutput = FindViewById<TextView> (Resource.Id.output);
+			Button evalButton = FindViewById<Button> (Resource.Id.evalbutton);
+
+			evalButton.Click += delegate {
+				EvaluateInput();
+			};
+		}
+
+		void EvaluateInput()
+		{
+			if (textviewinput.Text.Length == 0) {
+				textviewoutput.Text = "No Input!";
+			}
+
+			eval.Parse (textviewinput.Text);
+
+			var res = eval.Evaluate ();
+
+			if(!(res == null || res.GetType() == typeof(Error)))
+			{
+				textviewoutput.Text = res.ToString();
+			}
+		}
+	}
+}
+
+
