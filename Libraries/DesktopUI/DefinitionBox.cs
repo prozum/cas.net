6ba6b0c469e94addbf896f50fb3f84862ed20cfe
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-05-25 11:29:34 +0200

    [Lib/DesktopUI] Displays variables correct again

--- Libraries/DesktopUI/DefinitionBox.cs
+++ Libraries/DesktopUI/DefinitionBox.cs
@@ -34,7 +34,24 @@ namespace DesktopUI
 
             foreach (var def in Eval.Locals)
             {
-                list.AppendValues(def.Value.ToString(), def.Value.Value.ToString());
+                if(def.Value is SysFunc)
+                {
+                    list.AppendValues(def.Value.ToString(), "System Magic");
+                }
+                else if(def.Value is VarFunc)
+                {
+                    list.AppendValues(def.Value.ToString(), (def.Value as VarFunc).Definition.ToString());
+                }
+                else if(def.Value is Scope)
+                {
+                    list.AppendValues(def.Key, def.Value.ToString());
+                }
+                else
+                {
+                    list.AppendValues(def.Key, def.Value.Value.ToString());
+                }
+
+                //list.AppendValues(def.Value.ToString(), def.Value.Value.ToString());
             }
   
 			ShowAll ();
